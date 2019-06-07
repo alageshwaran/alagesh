a24020d605d626a59a7e3a54ce18225c496c2c2d
alageshwaran <42169721+alageshwaran@users.noreply.github.com>
2019-06-07 09:35:57 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,20 +1,6 @@
-import java.util.*;
- 
-class positive
-{
-    public static void main(String []args)
-    {
-        int num;
-        Scanner sc=new Scanner(System.in);
-         
-        System.out.print("Enter any integer number: ");
-        num=sc.nextInt();
-        if(num>0)
-            System.out.println(num + " is POSITIVE NUMBER.");
-        else if(num<0)
-            System.out.println(num + " is NEGATIVE NUMBER.");
-        else
-            System.out.println("IT's ZERO.");
-         
-    }
-}
+a=int(input())
+if a>0:
+    print('Positive')
+elif a<0:
+    print('Negative') 
+else:print('Zero')
